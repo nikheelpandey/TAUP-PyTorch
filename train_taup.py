bf928823d6481afe23a3cf00adef33e27b50fb6d
octo <nikhilpandey360@gmail.com>
2021-04-17 15:00:53 +0530

    some minor changes

--- train_taup.py
+++ train_taup.py
@@ -3,12 +3,13 @@ import os
 import time
 import torch 
 import numpy as np
+from lars import LARS
 from tqdm import tqdm 
 import torch.optim as optim
 from datetime import datetime 
 from knn_monitor import knn_monitor
 from tensorboardX import SummaryWriter
-from torch.optim.lr_scheduler import ExponentialLR
+from lr_scheduler import LR_Scheduler
 
 
 from logger import Logger
@@ -55,20 +56,29 @@ backbone= get_backbone(resnet18(pretrained=False))
 model = ContrastiveModel(backbone).to(device)
 loss_func  = ContrastiveLoss().to(device)
 
-
+# hyperparams
 features = 128
 batch_size = batch = 2048
 epochs = 25
 lr = 1e-4
 device_id = 0
-wt_decay  = 0.90
+weight_decay  = 1.e-6
+
 image_size = (32,32)
+momentum = 0.9
+
+warmup_epochs =  10
+warmup_lr  =     0
+base_lr =    0.3
+final_lr =   0
+num_epochs =     800 # this parameter influence the lr decay
+stop_at_epoch =  100 # has to be smaller than num_epochs
+batch_size =     256
+knn_monitor =    False # knn monitor will take more time
+knn_interval =   5
+knn_k =      200
 
 
-optimizer = optim.Adam(model.parameters(), 
-            lr=lr,
-            weight_decay=wt_decay) 
-scheduler = ExponentialLR(optimizer, gamma= wt_decay)
 
 min_loss = np.inf #ironic
 accuracy = 0
@@ -83,6 +93,19 @@ train_loader, memory_loader, test_loader = get_train_mem_test_dataloaders(
                 download=True )
 
 train_transform , test_transform = gpu_transformer(image_size)
+
+
+
+
+optimizer = LARS(model.named_modules(), lr=lr*batch_size/256, momentum=momentum, weight_decay=weight_decay)
+
+scheduler = LR_Scheduler(
+    optimizer, warmup_epochs, warmup_lr*batch_size/256,
+
+    num_epochs, base_lr*batch_size/256, final_lr*batch_size/256, 
+    len(train_loader),
+    constant_predictor_lr=True # see the end of section 4.2 predictor
+    )
 
 
 
@@ -103,14 +126,16 @@ for epoch in global_progress:
         loss.backward()
         optimizer.step()
         scheduler.step()
-        data_dict.update({'lr': scheduler.get_last_lr()[0]})
+        data_dict.update({'lr': scheduler.get_last_lr()})
         local_progress.set_postfix(data_dict)
         logger.update_scalers(data_dict)
     
     current_loss = data_dict['loss']
-    accuracy = knn_monitor(model.backbone, memory_loader, test_loader, 'cpu', hide_progress=True) 
-    data_dict['accuracy'] = accuracy
-    epoch_dict = {'epoch':epoch, 'accuracy':accuracy}
+
+    if epoch % knn_interval == 0: 
+        accuracy = knn_monitor(model.backbone, memory_loader, test_loader, 'cpu', hide_progress=True) 
+        data_dict['accuracy'] = accuracy
+    
     global_progress.set_postfix(data_dict)
     logger.update_scalers(data_dict)
     
@@ -122,4 +147,4 @@ for epoch in global_progress:
         torch.save({
         'epoch':epoch+1,
         'state_dict': model.state_dict() }, model_path)
-        print(f'Model saved at: {model_path}')
+        # print(f'Model saved at: {model_path}')
